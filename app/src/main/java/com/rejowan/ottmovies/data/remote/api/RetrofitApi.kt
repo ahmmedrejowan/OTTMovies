d6e9dc0e04bab4a73723716d8a5312b1f7a88030
ahmmedrejowan <ahmmedrejowan@gmail.com>
2024-12-06 19:16:03 +0600

    Retrofit Setup

--- app/src/main/java/com/rejowan/ottmovies/data/remote/api/RetrofitApi.kt
+++ app/src/main/java/com/rejowan/ottmovies/data/remote/api/RetrofitApi.kt
@@ -1,2 +1,32 @@
 package com.rejowan.ottmovies.data.remote.api
 
+import com.rejowan.ottmovies.constants.Config
+import com.rejowan.ottmovies.data.remote.responses.MovieSearchResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface RetrofitAPI {
+
+
+    @GET("/")
+    fun searchMovie(
+        @Query("s") search: String,
+        @Query("y") year: Int,
+        @Query("type") type: String = "movie",
+        @Query("page") page: Int = 1,
+        @Query("apikey") apiKey: String = Config.API_KEY
+    ) : Call<MovieSearchResponse>
+
+
+    @GET("/")
+    fun getMovieDetails(
+        @Query("i") imdbId: String = "",
+        @Query("t") title: String = "",
+        @Query("plot") plot: String = "short",
+        @Query("apikey") apiKey: String = Config.API_KEY
+    ) : Call<MovieSearchResponse>
+
+
+
+}
--- app/src/main/java/com/rejowan/ottmovies/data/remote/api/RetrofitClient.kt
+++ app/src/main/java/com/rejowan/ottmovies/data/remote/api/RetrofitClient.kt
@@ -1,4 +1,48 @@
 package com.rejowan.ottmovies.data.remote.api
 
-class RetrofitClient {
+import android.util.Log
+import com.google.gson.GsonBuilder
+import com.google.gson.Strictness
+import com.rejowan.ottmovies.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+
+object RetrofitClient {
+    private const val TAG = "RetrofitClient"
+    private val retrofitInstances = HashMap<String, Retrofit>()
+
+    fun getInstance(baseURL: String): RetrofitAPI? {
+        return try {
+            if (!retrofitInstances.containsKey(baseURL)) {
+                val builder =
+                    OkHttpClient.Builder()
+                        .writeTimeout(5, TimeUnit.MINUTES)
+                        .readTimeout(5, TimeUnit.MINUTES)
+                        .connectTimeout(5, TimeUnit.MINUTES)
+
+                if (BuildConfig.DEBUG) {
+                    val loggingInterceptor = HttpLoggingInterceptor()
+                    loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+                    builder.addInterceptor(loggingInterceptor)
+                }
+
+                val okHttpClient = builder.build()
+
+                val gson = GsonBuilder().setStrictness(Strictness.LENIENT).create()
+
+                val retrofit = Retrofit.Builder().baseUrl(baseURL).client(okHttpClient)
+                    .addConverterFactory(GsonConverterFactory.create(gson)).build()
+
+                retrofitInstances[baseURL] = retrofit
+            }
+            retrofitInstances[baseURL]?.create(RetrofitAPI::class.java)
+        } catch (e: Exception) {
+            Log.e(TAG, "getInstance: ${e.message}", e)
+            null
+        }
+    }
 }
